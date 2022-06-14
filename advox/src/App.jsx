330d240be95c615f02ec69fa7af563fab946731b
minhtung.le <minhtung11a7@gmail.com>
2022-06-15 01:30:07 +0700

    Initialize project

---
+++ advox/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import FavMusicListContainer from "./containers/FavMusicListContainer";
+
+function App() {
+  return (
+    <div className="App">
+      <FavMusicListContainer />
+    </div>
+  );
+}
+
+export default App;
